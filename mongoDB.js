e20aab918dc1b9c9f4041bafd40c9e6b043c8b4a
andreasbrake <andreas_brake@verizon.net>
2014-05-24 19:12:14 -0700

    update to mongoDB and fixed a bit of css

--- mongoDB.js
+++ mongoDB.js
@@ -1,10 +1,16 @@
 var mongoose = require('mongoose')
 var options = {
 	user: 'admin',
-	pass: 'dwZ63dAT_1kV'
+	pass: 'r9Ptnm4zL6a3'
 }
 var ip_addr = process.env.OPENSHIFT_MONGODB_DB_HOST   || '127.0.0.1';
 var port    = process.env.OPENSHIFT_MONGODB_DB_PORT || '27017';
 
-var uri = ip_addr + ':' + port + '/reports'
-exports.db = mongoose.createConnection(uri)
+if(ip_addr == '127.0.0.1'){
+	var uri = ip_addr + ':' + port + '/reports'
+	exports.db = mongoose.createConnection(uri)	
+}
+else{
+	var uri = ip_addr + ':' + port + '/broncohacks'
+	exports.db = mongoose.createConnection(uri, options)
+}
