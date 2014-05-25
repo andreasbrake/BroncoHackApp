1d1f66638461452cbf365189f131b1d40cd4168a
MarMar-1 <mariusm10@gmail.com>
2014-05-25 08:36:35 -0700

    formatting changes

--- static/js/pinInfo.js
+++ static/js/pinInfo.js
@@ -1,4 +1,5 @@
 var type = ""
+var address = ""
 var count = 0
 var descriptions = []
 var dates = []
@@ -6,13 +7,23 @@ var images = []
 var currIndex = 0
 
 function changeData(pin){
+	var geocoder = new google.maps.Geocoder();
+
 	type = pin.get("reportType")
 	count = pin.get("reportCount")
 	descriptions = pin.get("description")
 	dates = pin.get("datetime")
 	images = pin.get("images")
 
-	renderData(0)
+	geocoder.geocode({'latLng': pin.position}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			address=results[0].formatted_address;
+			renderData(0)
+		} else {
+			alert("Geocoder failed due to: " + status);
+		}
+	});
+	
 }
 function getNext(){
 	if(currIndex < count - 1)
@@ -24,6 +35,7 @@ function getPrevious(){
 }
 function renderData(index){
 	document.getElementById("type").innerHTML = type
+	document.getElementById("address").innerHTML = address.substring(0,address.length-5)
 	document.getElementById("count").innerHTML = count
 	document.getElementById("descr").innerHTML = descriptions[index]
 	var date = new Date(dates[index])
